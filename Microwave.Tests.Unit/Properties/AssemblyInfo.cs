14146992f2cd7a34411c7a150c89da0cdd114835
Tobias Holm <201608355@post.au.dk>
2020-04-26 10:27:02 +0200

    Changes asd

--- Microwave.Tests.Unit/Properties/AssemblyInfo.cs
+++ Microwave.Tests.Unit/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Microwave.Tests")]
+[assembly: AssemblyTitle("Microwave.Tests.Unit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Microwave.Tests")]
+[assembly: AssemblyProduct("Microwave.Tests.Unit")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
